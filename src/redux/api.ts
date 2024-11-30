618bc4fa3ec502f6a14377978367381d9850aa7e
arpitj007 <rptjngr@gmail.com>
2024-12-01 00:31:20 +0530

    Update

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,2 +1,5 @@
-export const BASE_API = "http://localhost:4000/api";
+export const BASE_API =
+  import.meta.env.VITE_ENV === "dev"
+    ? "http://localhost:4000/api"
+    : "https://backend-firefly.vercel.app/api";
 export type ApiStatus = "idle" | "loading" | "succeeded" | "failed";
